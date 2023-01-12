97fe752b0ee1b884661b903f480b61ef30f55607
BinhMinh371 <minhquynh191371@gmail.com>
2023-01-12 10:11:19 +0700

    update code after review

--- js/index.js
+++ js/index.js
@@ -8,72 +8,84 @@ const btnNext = document.querySelector(".next");
 const btnPrevious = document.querySelector(".previous");
 const slideMain = document.querySelector(".bodymobile_sliderMain");
 const slideitems = document.querySelectorAll(".bodymobile_sliderItem");
-const sliderWidth = slideitems[0].offsetWidth;
-const sliderLenght = slideitems.length;
-let positionX = 0;
-let index = 0;
 const increment = document.querySelector(".body_right-plus");
 const decrement = document.querySelector(".body_right-minus");
 const counterNumber = document.querySelector(".body_right-quantity");
+const activeImg = document.getElementsByClassName("active");
+const imgs = document.getElementsByClassName("img_product");
+const imgBig = document.querySelector(".img_big");
+const imgItem = document.querySelector(".body_left-imgitem");
+const sliderWidth = slideitems[0].offsetWidth;
+const sliderLenght = slideitems.length;
+const nextSlider = 1;
+const priviousSlider = -1;
+let counterValue = parseInt(counterNumber.textContent);
 let maxValue = 10;
 let minValue = 0;
-let counterValue = parseInt(counterNumber.textContent);
-let activeImg = document.getElementsByClassName("active");
-let imgs = document.getElementsByClassName("img_product");
-let imgBig = document.querySelector(".img_big");
-let imgItem = document.querySelector(".body_left-imgitem")
+let positionX = 0;
+let index = 0;
 
 // handle slide pc
-for (let i = 0; i < imgs.length; i++) {
-  imgs[i].addEventListener("mouseover", function()  {
+for (let index = 0; index < imgs.length; index++) {
+  imgs[index].addEventListener("mouseover", function () {
     this.classList.add("active");
     imgBig.src = this.src;
-    
   });
 }
 
 // handle slide mobile & tablet
 const handleChangeSlide = (dir) => {
-  if (dir === 1) {
+  if (dir === nextSlider) {
     if (index >= sliderLenght - 1) {
       index = sliderLenght - 1;
       return;
     }
-    positionX = positionX - sliderWidth;
+    positionX -= sliderWidth;
     slideMain.style = `transform:translateX(${positionX}px)`;
     index++;
-    console.log(slideMain);
-  } else if (dir === -1) {
+  } else if (dir === priviousSlider) {
     if (index <= 0) {
       index = 0;
       return;
     }
-    positionX = positionX + sliderWidth;
+    positionX += sliderWidth;
     slideMain.style = `transform:translateX(${positionX}px)`;
     index--;
+  } else {
+    return;
   }
 };
 
-btnNext.addEventListener("click", () => handleChangeSlide(1));
+btnNext.addEventListener("click", () => handleChangeSlide(nextSlider));
 
-btnPrevious.addEventListener("click", () => handleChangeSlide(-1));
+btnPrevious.addEventListener("click", () => handleChangeSlide(priviousSlider));
 
 // handle change quantity
-increment.addEventListener("click", () => {
-  counterValue >= maxValue ? maxValue : counterValue++;
-  counterValue >= maxValue
-    ? increment.classList.add("no-click")
-    : decrement.classList.remove("no-click");
+const handleIncrementQuantity = () => {
+  if (counterValue >= maxValue) {
+    counterValue = maxValue;
+    increment.classList.add("no-click");
+  } else {
+    counterValue++;
+    decrement.classList.remove("no-click");
+  }
   counterNumber.textContent = counterValue;
-});
+};
 
-decrement.addEventListener("click", () => {
-  counterValue == minValue ? minValue : counterValue--;
-  counterValue == minValue
-    ? decrement.classList.add("no-click")
-    : increment.classList.remove("no-click");
+const handleDecrementQuantity = () => {
+  if (counterValue == minValue) {
+    counterValue = minValue;
+    decrement.classList.add("no-click");
+  } else {
+    counterValue--;
+    increment.classList.remove("no-click");
+  }
   counterNumber.textContent = counterValue;
-});
+};
+
+increment.addEventListener("click", handleIncrementQuantity);
+
+decrement.addEventListener("click", handleDecrementQuantity);
 
 //  handle menu mobile
 btlmenu.addEventListener("click", () => {
